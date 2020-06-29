23a6a7215a116215c0523c43dd3916439f67ee45
Dave Kellermanns <dave.kellermanns@ca.com>
2020-06-29 13:36:52 -0600

    Update vars_all.tf

--- aws_agent/vars_all.tf
+++ aws_agent/vars_all.tf
@@ -1,5 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-0a54aef4ef3b5f881"}
 variable "aws_security_group_id" {default = "sg-ba4ebbc0"}
 variable "instance_type" {default = "t2.micro"}
