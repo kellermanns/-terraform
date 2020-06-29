e1d80d0c96b9727c17ac49765a29b4dbe5b6ab26
Dave Kellermanns <dave.kellermanns@ca.com>
2020-06-29 10:58:40 -0600

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -4,6 +4,7 @@ variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
+variable "vpc_id" {}
 
 provider "aws" {
   region     = "us-east-1"
@@ -13,6 +14,7 @@ provider "aws" {
 
 resource "aws_instance" "automic_default" {
   ami                    = "${var.aws_ami}"
+  vpc_id            = "${data.aws_vpc.selected.id}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${var.aws_security_group_id}"]
   key_name	             = "AWS Default"
