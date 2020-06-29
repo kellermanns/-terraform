e9031eb3832278494c73290a2e6a5d822f88ac2e
Dave Kellermanns <dave.kellermanns@ca.com>
2020-06-29 15:01:09 -0600

    Update vars_all.tf

--- aws_agent/vars_all.tf
+++ aws_agent/vars_all.tf
@@ -3,7 +3,7 @@ variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-0a54aef4ef3b5f881"}
 variable "aws_security_group_id" {default = "launch-wizard-1"}
 variable "instance_type" {default = "t2.micro"}
-variable "remote_working_dir" {default = "/home/ec2-user/AE"}
+variable "remote_working_dir" {default = "/home/ec2-user"}
 variable "private_key_file" {default = "C:\\Automic\\Terraform\\AWSDefault.pem"}
 variable "agent_port" {default = "2300"}
 variable "ae_system_name" {default = "AUTOMIC"}
