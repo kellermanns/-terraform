ceb4829f05ef10f2792b8401b292df06d1b0179f
Dave Kellermanns <dave.kellermanns@ca.com>
2020-06-29 11:04:39 -0600

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -4,22 +4,11 @@ variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
-variable "vpc_id" {}
-
-data "aws_vpc" "selected" {
-  id = "${var.vpc_id}"
-}
 
 provider "aws" {
   region     = "us-east-1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-}
-
-resource "aws_subnet" "example" {
-  vpc_id            = "${data.aws_vpc.selected.id}"
-  availability_zone = "us-west-2a"
-  cidr_block        = "${cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)}"
 }
 
 resource "aws_instance" "automic_default" {
