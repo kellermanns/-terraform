0eb6a723fec8089d8363f7a6b37360611e5bb26f
Dave Kellermanns <dave.kellermanns@ca.com>
2020-07-06 09:45:29 -0600

    Update main.tf

--- gc_agent/main.tf
+++ gc_agent/main.tf
@@ -3,7 +3,7 @@ variable "region" {default = "us-west1"}
 variable "subnetwork" {default = "test-network-sub"}
 variable "image" {default = "ubuntu-1604-xenial-v20190212"}
 variable "gc_credentials" {}
-variable "infrastructure_name" {default = "sales-report-power"}
+variable "infrastructure_name" {default = "big-sales-data"}
 variable "zone" {default = "us-west1-b"}
 
 
