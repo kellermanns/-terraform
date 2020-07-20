6f94cbf84fc6e68200d9ff455dbc830bfd1de9c5
Dave Kellermanns <dave.kellermanns@ca.com>
2020-07-20 09:13:03 -0600

    Update main.tf

--- aws_agent/main.tf
+++ aws_agent/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "automic_instance" {
   	ami                    = var.aws_ami
   	instance_type          = var.instance_type
-	vpc_security_group_ids = var.aws_security_group_id
+	vpc_security_group_ids = [var.aws_security_group_id]
   	key_name	         = "AWS"
 	
   	tags = {
