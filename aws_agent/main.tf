63fbd41b2b0c83dd78a2976a61b4439b9b42059b
Dave Kellermanns <dave.kellermanns@ca.com>
2020-07-20 09:00:53 -0600

    Update main.tf

--- aws_agent/main.tf
+++ aws_agent/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	region     = "us-east-2"
 	access_key = var.aws_access_key
-  	secret_key = var.aws_secret_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "automic_instance" {
@@ -23,18 +23,18 @@ resource "aws_instance" "automic_instance" {
     		ae_system_name = var.ae_system_name
     		ae_host = var.ae_host
     		ae_port = var.ae_port
-    		sm_port = var.sm_port
-    		sm_name = var.sm_name}${random_string.append_string.result
+    		sm_port = var.sm_port}"
+    		sm_name = "${var.sm_name}${random_string.append_string.result}"
 
     		variables = {
-      			UC_EX_IP_ADDR = self.public_ip}"
+      			UC_EX_IP_ADDR = "${self.public_ip}"
     		}
 
     		connection {
       			host = self.public_ip
       			type = "ssh"
       			user = "ubuntu"
-      			private_key = file(var.private_key_file)}"
+      			private_key = "${file(var.private_key_file)}"
     		}
   	}   
 }
