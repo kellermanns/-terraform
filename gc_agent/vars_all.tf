2abbad078abb0bdf265feb189a2c54c92d1347fb
Dave Kellermanns <dave.kellermanns@ca.com>
2021-02-04 15:47:11 -0700

    Update vars_all.tf

--- gc_agent/vars_all.tf
+++ gc_agent/vars_all.tf
@@ -3,6 +3,6 @@ variable "private_key_file" {default = "C:\\Automic\\Terraform\\gcp.key"}
 variable "agent_port" {default = "2300"}
 variable "ae_system_name" {default = "AUTOMIC"}
 variable "ae_host" {default = "7tqt640g88.d.firewalla.org"}
-variable "ae_port" {default = "2217"}
+variable "ae_port" {default = "2219"}
 variable "sm_port" {default = "8871"}
 variable "sm_name" {default = "sm_"}
